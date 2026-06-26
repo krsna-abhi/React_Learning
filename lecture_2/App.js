2b879253594844a3c052b4ecdedfee2908e3ad34
krsna <krishnarays.b@gmail.com>
2026-06-27 01:57:37 +0530

    my first react files & notes

---
+++ lecture_2/App.js
@@ -0,0 +1,13 @@
+// here we are creating a h1 element in React using its React.createElement method
+const titleElement = React.createElement("h1", null, "react to mast hai");
+// but this titleElement aka our React element is not yet rendered on the web page,
+
+// we need to use ReactDOM methods to render it on the web page
+// first - we will get the root element from the html page
+const rootElement = document.getElementById("root");
+// second - we will use ReactDOM.createRoot() method to create a root \ connection \ portal \ path for our React element to be rendered within the <div id="root">root</div>
+const path = ReactDOM.createRoot(rootElement); // here we are creating a path from our React app to the root element within the html page
+// third - we will use .render() method on our path variable to render our React element titleElement within the root element of the html page
+path.render(titleElement);
+
+// React App --> React-DOM Bridge --> Virtual Element --> Actual DOM Node --> web page ( root element within the html page )
