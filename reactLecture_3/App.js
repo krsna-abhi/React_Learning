88a019b2faf4beb595cd6d1f6e032119f1dd8231
krsna <krishnarays.b@gmail.com>
2026-06-29 02:38:55 +0530

    notes

--- reactLecture_3/App.js
+++ reactLecture_3/App.js
@@ -4,6 +4,7 @@ import reactDom from "react-dom/client";
 const root = document.getElementById("root");
 const path = reactDom.createRoot(root);
 
+// react element : --------------------------------------------------------------------
 /*
 const reactElement = react.createElement(
   "h1",
@@ -14,8 +15,32 @@ const reactElement = react.createElement(
 path.render(reactElement);
 */
 
+/*
+  // variable holding a react element
 const jscReactElement = (
-  <h1 id="heading">tera bhai react-js aur jsx paad raha hai</h1>
+  <h1 id="heading">tera bhai react paad raha hai</h1>
 );
 
 path.render(jscReactElement);
+
+  // const myElement  = <h1>...</h1>   // lowercase = variable, used as {myElement}
+  // const Heading = () => { ... }        // Capital = component, used as <Heading />
+*/
+
+// react functional component : --------------------------------------------------------------------
+const MyComponenet = () => {
+  return (
+    <div>
+      <h1>tera bhai react-js aur jsx paad raha hai</h1>
+      <p>tera bhai react-js aur jsx paad raha hai</p>
+    </div>
+  );
+};
+
+path.render(<MyComponenet />);
+/* this will be returned by the MyComponent functional component :
+  <div>
+    <h1>tera bhai react-js aur jsx paad raha hai</h1>
+    <p>tera bhai react-js aur jsx paad raha hai</p>
+  </div>
+*/
