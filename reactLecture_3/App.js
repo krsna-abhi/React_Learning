0151b90e07eaeb07cbc09859de7121027e07ed4e
krsna <krishnarays.b@gmail.com>
2026-06-28 23:34:56 +0530

    learning

--- reactLecture_3/App.js
+++ reactLecture_3/App.js
@@ -3,3 +3,11 @@ import reactDom from "react-dom/client";
 
 const root = document.getElementById("root");
 const path = reactDom.createRoot(root);
+
+const reactElement = react.createElement(
+  "h1",
+  { id: "heading" },
+  "tera bhai react paad raha hai",
+);
+
+path.render(reactElement);
