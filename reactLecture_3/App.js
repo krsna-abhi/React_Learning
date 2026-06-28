f652ca5f5574c3e55855457bd3c0f55c26475724
krsna <krishnarays.b@gmail.com>
2026-06-29 00:34:28 +0530

    code changes & notes

--- reactLecture_3/App.js
+++ reactLecture_3/App.js
@@ -4,10 +4,23 @@ import reactDom from "react-dom/client";
 const root = document.getElementById("root");
 const path = reactDom.createRoot(root);
 
+// react element is a object which is created by react.createElement() function.
+// It is used to create a virtual DOM element in React.
+// it returns a js object, which represents the virtual DOM element and this virtual DOM element can be rendered
+// into the actual DOM using ReactDOM package.
+
+/*
 const reactElement = react.createElement(
   "h1",
   { id: "heading" },
   "tera bhai react paad raha hai",
 );
+*/
 
 path.render(reactElement);
+
+const jscReactElement = (
+  <h1 id="heading">tera bhai react-js aur jsx paad raha hai</h1>
+);
+
+path.render(jscReactElement);
